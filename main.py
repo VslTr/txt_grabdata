c4d55432964df532fc616a231cb3b41f0852ee2e
Vyacheslav Tretyakov <tri.vyacheslav@gmail.com>
2019-12-18 16:36:31 +0300

    add sorted list

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 data_list = []
 
-with open('Text.txt', 'r') as f:
+with open('data.txt', 'r') as f:
     for i in f:
-        if len(i) == 11:  # выбираем строку (по длинне) с обьемом заявки
+        # print(len(i))
+        if len(i) == 19:  # выбираем строку (по длинне) с обьемом заявки
             j = i.replace('\n', '')
             data_list.append(j.split())
 
-print(data_list)
+print(f'{data_list[0:3]= }')  # Смотрим результат в срезе от 0-го до 3-го ел-та
+sorted_list = [sorted(i) for i in data_list]  # Новый с-ок с доваблением в него отсортировонных ел-в из с-ка "data_list"
+print(f'{sorted_list[0:3]= }')  # Смотрим результат в срезе
+
